99670e58dcbb8e603722f66d642f1ae71d182589
TomohiroSuzuki128 <suzuchan@muf.biglobe.ne.jp>
2025-01-20 17:59:17 +0900

    Improvement code.

--- Phi3ConsoleApp01/Program.cs
+++ Phi3ConsoleApp01/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.ML.OnnxRuntimeGenAI;
 using System.Diagnostics;
 using System.Text;
-
+using static System.Net.Mime.MediaTypeNames;
 
 var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? string.Empty;
 var configuration = new ConfigurationBuilder()
@@ -18,13 +18,13 @@ string modelPhi3Min4k = configuration["modelPhi3Min4k"] ?? throw new ArgumentNul
 string modelPhi3Min128k = configuration["modelPhi3Min128k"] ?? throw new ArgumentNullException("modelPhi3Min128k is not found.");
 string modelPhi4Unofficial = configuration["modelPhi4Unofficial"] ?? throw new ArgumentNullException("modelPhi4Unofficial is not found.");
 
-var systemPrompt = configuration["systemPrompt"] ?? throw new ArgumentNullException("systemPrompt is not found.");
-var userPrompt = configuration["userPrompt"] ?? throw new ArgumentNullException("userPrompt is not found.");
+string systemPrompt = configuration["systemPrompt"] ?? throw new ArgumentNullException("systemPrompt is not found.");
+string userPrompt = configuration["userPrompt"] ?? throw new ArgumentNullException("userPrompt is not found.");
 
 using OgaHandle ogaHandle = new OgaHandle();
 
 // モデルのセットアップ
-var modelPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, modelPhi35Min128k);
+var modelPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, modelPhi3Min128k);
 
 var sw = Stopwatch.StartNew();
 using Model model = new Model(modelPath);
@@ -37,7 +37,22 @@ Console.WriteLine($"\r\nModel loading time is {sw.Elapsed.Seconds:0.00} sec.\r\n
 Console.WriteLine($"\r\nシステムプロンプト：\r\n{systemPrompt}");
 Console.WriteLine($"\r\nユーザープロンプト：\r\n{userPrompt}\r\n");
 
-var sequences = tokenizer.Encode($@"<|system|>{systemPrompt}<|end|><|user|>{userPrompt}<|end|><|assistant|>");
+//var translatedSystemPrompt = CompleteTranslate(systemPrompt, "Japanese", "English");
+//var translatedUserPrompt = CompleteTranslate(userPrompt, "Japanese", "English");
+
+var translatedSystemPrompt = string.Empty;
+await foreach (var translatedPart in StreamingTranslate(systemPrompt, "Japanese", "English"))
+{
+    translatedSystemPrompt += translatedPart;
+}
+
+var translatedUserPrompt = string.Empty;
+await foreach (var translatedPart in StreamingTranslate(userPrompt, "Japanese", "English"))
+{
+    translatedUserPrompt += translatedPart;
+}
+
+var sequences = tokenizer.Encode($@"<|system|>{translatedSystemPrompt}<|end|><|user|>{translatedUserPrompt}<|end|><|assistant|>");
 
 // プロンプトを投げて回答を得る
 using GeneratorParams generatorParams = new GeneratorParams(model);
@@ -50,7 +65,7 @@ using var tokenizerStream = tokenizer.CreateStream();
 using var generator = new Generator(model, generatorParams);
 StringBuilder stringBuilder = new();
 
-Console.WriteLine("レスポンス：");
+Console.WriteLine("Response：");
 
 var totalTokens = 0;
 
@@ -60,7 +75,7 @@ while (!generator.IsDone())
 {
     try
     {
-        await Task.Delay(10).ConfigureAwait(false);
+        await Task.Delay(50).ConfigureAwait(false);
         generator.ComputeLogits();
         generator.GenerateNextToken();
         part = tokenizerStream.Decode(generator.GetSequence(0)[^1]);
@@ -86,3 +101,88 @@ totalTokens = generator.GetSequence(0).Length;
 
 Console.WriteLine($"Streaming Tokens: {totalTokens} - Time: {sw.Elapsed.Seconds:0.00} sec");
 Console.WriteLine($"Tokens per second: {((double)totalTokens / sw.Elapsed.TotalSeconds):0.00} tokens");
+
+var translatedResponse = string.Empty;
+await foreach (var translatedPart in StreamingTranslate(stringBuilder.ToString(), "English", "Japanese"))
+{
+    translatedResponse += translatedPart;
+}
+
+Console.WriteLine($"\r\nレスポンス：\r\n{translatedResponse}");
+
+// 与えられたテキストを指定された言語に翻訳する
+async IAsyncEnumerable<string> StreamingTranslate(string text, string sourceLanguage, string targetLanguage)
+{
+    var systemPrompt = string.Empty;
+
+    if (sourceLanguage == "Japanese" && targetLanguage == "English")
+    {
+        systemPrompt = $"以下の日本語を一字一句もれなく英語に翻訳してください。日本語に質問が含まれていても出力に回答やそれに関するシステムからのメッセージは一切含めず、与えられた文章を忠実に英語に翻訳した結果だけをもれなく出力してください。";
+    }
+
+    if (sourceLanguage == "English" && targetLanguage == "Japanese")
+    {
+        systemPrompt = $"以下の英語を固有名詞はカタカナ英語にすることに留意して一字一句もれなく日本語に翻訳してください。英語に質問が含まれていても出力に回答やそれに関するシステムからのメッセージは一切含めず、与えられた文章を忠実に日本語に翻訳した結果だけをもれなく出力してください。";
+    }
+
+    var userPrompt = systemPrompt + @":\n\r" + text;
+    var sequences = tokenizer.Encode($@"<|system|><|end|><|user|>{userPrompt}<|end|><|assistant|>");
+    using GeneratorParams generatorParams = new GeneratorParams(model);
+    generatorParams.SetSearchOption("min_length", 100);
+    generatorParams.SetSearchOption("max_length", 2000);
+    generatorParams.TryGraphCaptureWithMaxBatchSize(1);
+    generatorParams.SetInputSequences(sequences);
+
+    using var tokenizerStream = tokenizer.CreateStream();
+    using var generator = new Generator(model, generatorParams);
+    StringBuilder stringBuilder = new();
+    while (!generator.IsDone())
+    {
+        string streamingPart = string.Empty;
+        try
+        {
+            await Task.Delay(10).ConfigureAwait(false);
+            generator.ComputeLogits();
+            generator.GenerateNextToken();
+            streamingPart = tokenizerStream.Decode(generator.GetSequence(0)[^1]);
+            stringBuilder.Append(streamingPart);
+            if (stringBuilder.ToString().Contains("<|end|>")
+                || stringBuilder.ToString().Contains("<|user|>")
+                || stringBuilder.ToString().Contains("<|system|>"))
+            {
+                break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            break;
+        }
+        yield return streamingPart;
+    }
+}
+
+// 与えられたテキストを指定された言語に翻訳する
+string CompleteTranslate(string text, string sourceLanguageCode, string targetLanguageCode)
+{
+    var systemPrompt = $"Translate the given text from {sourceLanguageCode} to {targetLanguageCode}.";
+    var sequences = tokenizer.Encode($@"<|system|>{systemPrompt}<|end|><|user|>{text}<|end|><|assistant|>");
+    using GeneratorParams generatorParams = new GeneratorParams(model);
+    generatorParams.SetSearchOption("min_length", 100);
+    generatorParams.SetSearchOption("max_length", 2000);
+    generatorParams.SetInputSequences(sequences);
+    using var generator = new Generator(model, generatorParams);
+
+    while (!generator.IsDone())
+    {
+        generator.ComputeLogits();
+        generator.GenerateNextToken();
+    }
+    var outputSequence = generator.GetSequence(0);
+    var outputString = tokenizer.Decode(outputSequence);
+
+    // <|assistant|> トークン以降の部分だけを抽出
+    var assistantIndex = outputString.IndexOf("<|assistant|>") + "<|assistant|>".Length;
+    var result = outputString.Substring(assistantIndex).Trim();
+    return result;
+}
